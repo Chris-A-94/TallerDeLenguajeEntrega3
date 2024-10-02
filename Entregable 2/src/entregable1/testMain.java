03336f2973e568de5fbb80cbdc8d27d3b9c34a77
Chris <almeidachristian94@hotmail.com>
2024-10-02 16:28:27 -0300

    Agregue metodo generarStock
    Genera un numero random de monedas al inicializar.

---
+++ Entregable 2/src/entregable1/testMain.java
@@ -0,0 +1,14 @@
+package entregable1;
+
+public class testMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Coin bitcoin = new Coin("Bitcoin","BTC", 60000.3);
+		
+		
+		
+		System.out.println("Stock:: "+bitcoin.getStock());
+	}
+
+}
