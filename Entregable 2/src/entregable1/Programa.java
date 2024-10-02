b9d161ac10bfa7afdd73a1dd886262d0aa441292
Fran <ch.francisco.fc@gmail.com>
2024-10-02 16:15:41 -0300

    test

---
+++ Entregable 2/src/entregable1/Programa.java
@@ -0,0 +1,11 @@
+package entregable1;
+
+public class Programa {
+	public static void main(String[] args) {
+		boolean cerrar = false;
+		
+		while (!cerrar) {
+			System.out.printf("Test");
+		}
+	}
+}
