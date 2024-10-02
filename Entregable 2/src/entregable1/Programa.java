4801e95908f499c13e42d0b41dd1fbdde5ed2194
Fran <ch.francisco.fc@gmail.com>
2024-10-02 16:43:53 -0300

    Ahora responde ante la opción seleccionada.

--- Entregable 2/src/entregable1/Programa.java
+++ Entregable 2/src/entregable1/Programa.java
@@ -27,6 +27,30 @@ public class Programa {
 			opt = in.nextInt();
 			
 			System.out.printf("[%d]\n", opt);
+			
+			switch (opt) {
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+			case _EXIT:
+				break;
+			default:
+				System.out.printf("Opción incorrecta\n");
+				break;
+			}
 		} while (opt != _EXIT);
 		
 		
