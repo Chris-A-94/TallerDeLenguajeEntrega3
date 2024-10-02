8cd7e2de277cb721d8ff39f7a464153058c7da3d
AgustinMoyanoD <agustinmoyano3101@gmail.com>
2024-10-02 15:59:49 -0300

    Prueba de commit con la clase de Sistem implementada

---
+++ Entregable 2/src/entregable1/Sistema.java
@@ -0,0 +1,9 @@
+package entregable1;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Sistema {
+	List<Coin> monedas = new LinkedList<Coin>();
+	List<BlockChain> blockChain = new LinkedList<BlockChain>();
+}
