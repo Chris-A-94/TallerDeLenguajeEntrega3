d61ef8c3e1f917b2f85d58dcce1754456da385d2
Fran <ch.francisco.fc@gmail.com>
2024-10-02 16:34:24 -0300

    Se crea la base del programa principal, donde se imprime en pantalla las opciones y se lee de teclado.

--- Entregable 2/src/entregable1/Programa.java
+++ Entregable 2/src/entregable1/Programa.java
@@ -1,11 +1,35 @@
 package entregable1;
 
+import java.util.Scanner;
+
 public class Programa {
 	public static void main(String[] args) {
-		boolean cerrar = false;
+		final int _EXIT = 9;
 		
-		while (!cerrar) {
-			System.out.printf("Test");
-		}
+		final Scanner in = new Scanner(System.in);
+		
+		//System sistema = new System();
+		
+		Integer opt = -1;
+		
+		do {
+			System.out.printf("Seleccionar opción: \n"
+					+ "1. Crear moneda \n"
+					+ "2. Listar monedas \n"
+					+ "3. Generar Stock \n"
+					+ "4. Listar Stock \n"
+					+ "5. Generar Activos \n"
+					+ "6. Listar Mis Activos \n"
+					+ "7. Simular una compra \n"
+					+ "8. Simular Swap \n"
+					+ "9. Cerrar\n"
+					+ "opt: ");
+			opt = in.nextInt();
+			
+			System.out.printf("[%d]\n", opt);
+		} while (opt != _EXIT);
+		
+		
+		
 	}
 }
