a8bab29d255413d74089b5723cb9a9c26cf4b580
Fran <ch.francisco.fc@gmail.com>
2024-10-21 17:36:44 -0300

    Se arregló y se escribieron comentarios en '.optGenerarActivos()', se agregó un nuevo metodo que realiza una búsqueda sobre la base de datos.

--- Entregable 2/src/entregable1/Programa.java
+++ Entregable 2/src/entregable1/Programa.java
@@ -2,33 +2,69 @@ package entregable1;
 
 import java.util.Scanner;
 import java.util.LinkedList;
+import java.util.List;
+
+import daos.CoinDAO;
 
 public class Programa {
+	private static boolean existeMoneda(String sigla) {
+		boolean existe = false;
+		
+		CoinDAO myCoin = new CoinDAO();
+		List<Coin> monedas = new LinkedList<Coin>();
+		monedas.addAll(myCoin.devolverTabla());
+		
+		for (Coin coin : monedas) {
+			if (coin.getSigla().equals(sigla)) {
+				existe = true;
+				break;
+			}
+		}
+		
+		return existe;
+	}
+	
 	private static void optGenerarActivos(Usuario user) {
 			Scanner in = new Scanner(System.in);
 			
 			System.out.printf("[Generar Activos]\n"
-					+ "Introduzca el nombre de la moneda (* para seleccionar todas): ");
+					+ "Introduzca las siglas de la moneda (* para seleccionar todas): ");
 			String sigla = in.next();
 			System.out.printf("Introduzca la cantidad de monedas: ");
 			Double cantidad = in.nextDouble();
 			
+			// Actualizar el saldo en todas las monedas
 			if (sigla.equals("*")) {
-				for (Saldo saldo : user.getBilletera().getArregloMontos()) {
+				for (Saldo saldo : user.getBilletera().getArregloSaldo()) {
 					saldo.setCantMonedas(cantidad);
 				}
+			// Actualizar unicamente la moneda indicada
 			} else {
-				for (Saldo saldo : user.getBilletera().getArregloMontos()) {
+				// Busca si la moneda existe en la base de datos
+				
+				if (!existeMoneda(sigla)) {
+					System.out.printf("ERROR::DB::LA_MONEDA_NO_EXISTE\n");
+					return;
+				}
+				// Se busca la moneda en arregloSaldo[]
+				for (Saldo saldo : user.getBilletera().getArregloSaldo()) {
 					if (saldo.getSigla().equals(sigla)) {
 						saldo.setCantMonedas(cantidad);
-						break;
+						return;
 					}
 				}
+				
+				// Si existe en la base de datos y no fue encontrada dentro de arregloSaldo[]...
+				// Agrega la moneda al arregloSaldo[]
+				// Nota: Es medio redundante hacer esto pero existe la posibilidad de que la moneda exista en la base de datos
+				// y que no se encuentre instanciada un 'saldo' dentro de la billetera del usuario, para quitarle responsabilidad 
+				// a '.optCrearMoneda()', se agrega un paso extra con el fin de evitar un posible error en el futuro.
+				user.getBilletera().getArregloSaldo().add(new Saldo(sigla, cantidad));
 			}
 	}
 	private static void optListarActivos(Usuario user) {
 		LinkedList<Saldo> list = new LinkedList<Saldo>();
-		for (Saldo saldo : user.getBilletera().getArregloMontos()) {
+		for (Saldo saldo : user.getBilletera().getArregloSaldo()) {
 			list.add(saldo);
 		}
 		list.sort(null);
