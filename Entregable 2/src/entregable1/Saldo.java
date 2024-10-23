231196be8aa417e1b50315669f120744adc4aca8
Chris <almeidachristian94@hotmail.com>
2024-10-23 10:47:06 -0300

    Push temporal para compartir codigo
    Cambios en Programa, Billetera, y Saldo. Todo incompleto.

--- Entregable 2/src/entregable1/Saldo.java
+++ Entregable 2/src/entregable1/Saldo.java
@@ -4,12 +4,25 @@ package entregable1;
  * Informa el balance de una cryptomoneda particular. Traduce el balance a USD.
  */
 public class Saldo implements Comparable<Saldo>{
+	
 	private String sigla = "?";
 	private Double cantMonedas = 0.0;
-	public Saldo(String sigla, Double cantMonedas) {
+	private Double precio;
+	private String nombre;
+	
+	public Saldo(String sigla, Double cantMonedas)
+	{
 		super();
 		this.sigla = sigla;
 		this.cantMonedas = cantMonedas;
+	}
+	public Saldo(String sigla, Double cantMonedas, Double precio, String nombre)
+	{
+		super();
+		this.sigla = sigla;
+		this.cantMonedas = cantMonedas;
+		this.precio = precio;
+		this.nombre = nombre;
 	}
 	public Saldo() {
 		super();
@@ -30,10 +43,15 @@ public class Saldo implements Comparable<Saldo>{
 		String aux = (sigla +" "+cantMonedas);
 		return aux;
 	}
-	public String ConvertirADolar(Double cantMonedas2) {
+	public Double ConvertirADolar(Double fiat) {
 		// Aca se convierte a pesos.
 		return null;
 	}
+	public Double dolarADivisa(Double dolar) {
+		// Aca se convierte a pesos.
+		return null;
+	}
+	
 	
 	@Override
 	public int compareTo(Saldo s) {
