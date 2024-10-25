b39ea8aad10c71eb4766f17ebd065a77030fef86
F. F. Chavez <ch.francisco.fc@gmail.com>
2024-10-25 01:06:21 -0300

    BASE_ENTREGABLE.db ya no es necesario

--- Entregable 2/src/entregable1/TipoMoneda.java
+++ Entregable 2/src/entregable1/TipoMoneda.java
@@ -1,7 +1,17 @@
 package entregable1;
 
 public enum TipoMoneda {
-	FIAT,
-	CRIPTOMONEDA,
-	UNDEFINED
+	FIAT ("FIAT"),
+	CRIPTOMONEDA ("CRIPTOMONEDA"),
+	UNDEFINED ("UNDEFINED");
+	
+	private final String name;
+	
+	private TipoMoneda(String s) {
+		name = s;
+	}
+	
+	public String toString() {
+		return this.name;
+	}
 }
