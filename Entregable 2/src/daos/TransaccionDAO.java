e76f218e167b781339e3d9c3706bcbc5874ad865
Fran <ch.francisco.fc@gmail.com>
2024-10-26 15:48:08 -0300

    Descenso a la locura

---
+++ Entregable 2/src/daos/TransaccionDAO.java
@@ -0,0 +1,91 @@
+package daos;
+
+import java.util.List;
+import java.util.LinkedList;
+
+import java.sql.*;
+
+import entregable1.Transaccion;
+
+public class TransaccionDAO implements DaoInterface<Transaccion> {
+	private Connection con = null;
+	
+	public TransaccionDAO() {
+		con = MyConnection.getCon();
+		this.crearTabla();
+	}
+	
+	public void crearTabla() {
+		try {
+			String query = "CREATE TABLE IF NOT EXISTS TRANSACCION (" +
+                    "ID INTEGER PRIMARY KEY AUTOINCREMENT," +
+                    "TEXTO TEXT NOT NULL," +
+                    "USER_ID TEXT NOT NULL," +
+                    "FOREIGN KEY ('USER_ID') REFERENCES 'USUARIOS' ('DNI')" +
+                    ");";
+			Statement pstmt = con.createStatement();
+			pstmt.execute(query);
+			pstmt.close();
+		} catch (SQLException e) {
+			System.out.printf("%s\n", e.toString());
+		}
+	}
+	@Override
+	public void modificar(Transaccion t) {
+		try {
+			String query = ("UPDATE TRANSACCION SET TEXTO = ?;");
+			PreparedStatement pstmt = con.prepareStatement(query);
+		    
+		  pstmt.setString(1,t.toString());
+		  pstmt.executeUpdate();
+		  pstmt.close();
+		  
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	@Override
+	public void guardar(Transaccion t) {
+			try {
+			    String query = "INSERT INTO TRANSACCION (TEXTO, USER_ID) VALUES (?, ?)";
+			    PreparedStatement pstmt = con.prepareStatement(query);
+			    
+			    pstmt.setString(1, t.toString());
+			    pstmt.setString(2, t.getUserID());
+			    
+			    pstmt.executeUpdate();
+			    pstmt.close();
+			  
+			} catch (SQLException e) {
+				    System.out.println(e.getMessage());
+			}
+}
+	@Override
+	public List<Transaccion> devolverTabla() {
+		List<Transaccion> list = new LinkedList<Transaccion>();
+		
+//		try {
+//			Statement sent = con.createStatement();	
+//			ResultSet resul = sent.executeQuery("SELECT * FROM TRANSACCION");
+//			
+//			while (resul.next() == true) {
+//				list.add(new String(resul.getString("TEXTO")));
+//			}
+//			
+//			resul.close();
+//			sent.close();
+//		} catch (SQLException e) {
+//			System.out.println(e.getMessage());
+//		}
+//
+// 	Dado que en la base de datos solo se almacena una descripción limitada, 
+//	no es posible obtener los datos suficientes para instanciar una Transacción.
+		
+		return list;
+	}
+	
+	@Override
+	public void remover(String s) {
+		// No fue implementado.
+	}
+}
