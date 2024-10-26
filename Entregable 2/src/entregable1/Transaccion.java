0ea87b0be643eb1b4960f8c8163eba33343e0659
Fran <ch.francisco.fc@gmail.com>
2024-10-26 10:22:14 -0300

    'Transaccion' ahora es una clase abstracta
    Ademas se realizaron los cambios necesarios dentro de la clase.

--- Entregable 2/src/entregable1/Transaccion.java
+++ Entregable 2/src/entregable1/Transaccion.java
@@ -1,44 +1,41 @@
 package entregable1;
-import java.time.*;
 
 /**
  *  Esta clase se instancia cuando el usuario realiza una operación de compra, venta o "swap". contiene los datos de la transacción.
  */
-public class Transaccion {
+abstract class Transaccion {
 	private Fecha fecha;
-	private TipoTransaccion tipo;
+	private TipoTransaccion tipo = TipoTransaccion.UNDEFINED;
 	private Double monto;
-	public Transaccion(TipoTransaccion tipo, Double monto,int dia,int mes,int year) {
-		//super(); no se porque estaba esto aca
-		this.fecha = new Fecha(dia,mes,year);
-		this.tipo = tipo;
+	
+	public Transaccion(Double monto, int dia, int mes, int year) {
+		this.fecha = new Fecha(dia, mes, year);
 		this.monto = monto;
 	}
-	public void transaccionCompra()
-	{
-		
-	}
+	
 	public String getFecha() {
 		return fecha.toString();
-	}
-	public void setFecha(int dia,int mes,int year) {
-		this.fecha = new Fecha(dia,mes,year);
 	}
 	public TipoTransaccion getTipo() {
 		return tipo;
 	}
-	public void setTipo(TipoTransaccion tipo) {
-		this.tipo = tipo;
-	}
 	public Double getMonto() {
 		return monto;
+	}
+	
+	public void setFecha(int dia, int mes, int year) {
+		this.fecha = new Fecha(dia,mes,year);
+	}
+	public void setTipo(TipoTransaccion tipo) {
+		this.tipo = tipo;
 	}
 	public void setMonto(Double monto) {
 		this.monto = monto;
 	}
+	
+	@Override
 	public String toString()
 	{
-		String aux = (fecha.toString()+" "+this.monto+" "+this.tipo);
-		return aux;
+		return new String("El dia [" + this.fecha.toString() + "] se realizó una transferencia de tipo " + this.getTipo().toString() + ", donde: ");
 	}
 }
