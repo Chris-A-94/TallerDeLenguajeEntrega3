67f6f9702d33faf821b36bb7e39aa4fd5e974f03
Chris <almeidachristian94@hotmail.com>
2024-11-05 10:34:25 -0300

    Reimaginacion de monitoreoCoin
    Implementa interprete JSON, y ahora retorna valores en funciones simples, de a uno o en multitud.

--- Entregable 2/src/entregable1/MonitoreoCoin.java
+++ Entregable 2/src/entregable1/MonitoreoCoin.java
@@ -1,121 +1,123 @@
 package entregable1;
 import java.io.BufferedReader;
+import org.json.*;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Implementacion funcional de monitorio de precios de cryptomoneda. Actualiza los precios de las 4 monedas a traves de una llamada al servidor CoinGecko.
  */
 public class MonitoreoCoin {
 	
-	private String apiURL;
-	private ArrayList<ArrayList<String>> monedasValue;
-	private int cantMonedas;
+	private String apiURL;	
+	private JSONObject monedasPrincipales;
 		
 	public MonitoreoCoin()
 	{
-		this.apiURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cusd-coin%2Cdogecoin&vs_currencies=usd&x_cg_demo_api_key=CG-wqRNzUCU4erPp2ahwamhe1f7";
-		monedasValue = new ArrayList<ArrayList<String>>();
-		this.cantMonedas = 5;
-		cargarNombre();
-		updatePrices();
-	}
-	
-	public ArrayList<ArrayList<String>> actualizarMonedas()
-	{
-		updatePrices();
-		return this.monedasValue;
-	}
-	
-	private void cargarNombre()
-	{	
-		for (int i = 0; i < cantMonedas; i++) {  
-	        this.monedasValue.add(new ArrayList<String>());  
-	    }
-		
-		this.monedasValue.get(0).add("bitcoin");
-		this.monedasValue.get(1).add("dogecoin");
-		this.monedasValue.get(2).add("etherum");
-		this.monedasValue.get(3).add("usdt");
-		this.monedasValue.get(4).add("usdc");
-		for(int i = 0; i < monedasValue.size(); i++)
-		{
-			this.monedasValue.get(i).add("0");
+		this.apiURL = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Cethereum%2Ctether%2Cusd-coin%2Cdogecoin&vs_currencies=usd&x_cg_demo_api_key=CG-wqRNzUCU4erPp2ahwamhe1f7";	
+		try {
+			updateMonedasPrincipales();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 	
-	private void updatePrices()
+	private void updateMonedasPrincipales() throws IOException
 	{
+		//carga la URL y se conecta con coingecko
+		URL direccion = new URL(apiURL);	
+		//openConnection retorna una URLConection, que es clase padre de HttpURLConnection
+		HttpURLConnection conexion = (HttpURLConnection) direccion.openConnection();
 		
-		try 
+		//seteo la conexion para conseguir data en formato JSON
+		conexion.setRequestMethod("GET");
+		conexion.setRequestProperty("Accept", "application/json");
+		if(conexion.getResponseCode() == 200)
 		{
-			//carga la URL y se conecta con coingecko
-			URL direccion = new URL(apiURL);	
-			//openConnection retorna una URLConection, que es clase padre de HttpURLConnection
-			HttpURLConnection conexion = (HttpURLConnection) direccion.openConnection();
-			
-			//seteo la conexion para conseguir data en formato JSON
-			conexion.setRequestMethod("GET");
-			conexion.setRequestProperty("Accept", "application/json");
-			
-			if(conexion.getResponseCode() == 200)
-			{
-				//Obtengo la InputStream de data y la convierto a una clase con metodos de lectura rapidos
-				BufferedReader rawData = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
-				String Data = rawData.readLine();
-				//elimino data irrelevante
-				String regex = "[\":}]";
-			    String[] valoresRaw = Data.split(regex);
-			    ArrayList<String> valores = new ArrayList<String>();
-			    //aislo los numeros
-			    for(String Raw: valoresRaw)
-			    {
-			    	System.out.print(Raw);
-			    	if(Raw.matches("[0-9]+(\\.[0-9]+)?"))
-			    		valores.add(Raw);
-			    } 			    
-			    
-			
-			    int cont = 0;
-			    for(String attempt: valores)
-			    {
-			    	//carga de datos en forma de string
-			    	System.out.println(attempt);
-			    	this.monedasValue.get(cont).remove(1);
-			    	this.monedasValue.get(cont).add(attempt);
-			    	cont++;
-			    }
-				
-				
-			}
-			
-			
+			//Obtengo la InputStream de data y la convierto a una clase con metodos de lectura rapidos
+			BufferedReader rawData = new BufferedReader(new InputStreamReader(conexion.getInputStream()));
+			String Data = rawData.readLine();			
+			JSONTokener interpret = new JSONTokener(Data);					
+			this.monedasPrincipales = new JSONObject(interpret);						
 		}
-		//capturamiento de posibles errores desde la web
-		catch (MalformedURLException e) 
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			} 
-		catch (ProtocolException e) 
-	    {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		   	} 
-		catch (IOException e)
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			}
-		
 		
 	}
+
+	public List<String> getIDS()
+	{
+		Set<String> keys = (Set<String>) this.monedasPrincipales.keySet();
+		List<String> names = new ArrayList<String>(keys);		
+		return names;
+	}
+	
+	public List<Double> getPrices()
+	{
+		try {
+			this.updateMonedasPrincipales();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		List<String> IDS = this.getIDS();
+		List<Double> precios = new ArrayList<Double>();
+		for(String ID: IDS)
+		{
+			JSONObject moneda = this.monedasPrincipales.getJSONObject(ID);
+			precios.add(moneda.getDouble("usd"));
+		}
+		return precios;		
+	}
+	
+	private boolean coinExists(String name)
+	{
+		List<String> names = this.getIDS();
+		boolean exists = false;
+		for(String aux: names)
+			if(aux.equals(name))
+				exists = true;
+		return exists;
+	}			
 	
 	
+	public Double getPrice(String name)
+	{
+		if(!coinExists(name))
+			return -1.0; //excepcion a futuro
+		
+		try {
+			this.updateMonedasPrincipales();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		JSONObject moneda = this.monedasPrincipales.getJSONObject(name);
+		Double precio = moneda.getDouble("usd");
+		return precio;
+	}		
+	
+	public String toString()
+	{
+		List<String> IDs = this.getIDS();
+		List<Double> precios = this.getPrices();
+		String aRetorno = "";
+		if(IDs.size() != precios.size())
+		{
+			System.err.println("Error fatal.");
+			return null;
+		}
+		for(int i = 0; i < precios.size(); i++)
+		{
+			aRetorno = "\n La moneda "+IDs.get(i)+" cuesta: "+precios.get(i)+" dolares.";
+		}
+		return aRetorno;
+	}
 
 }
