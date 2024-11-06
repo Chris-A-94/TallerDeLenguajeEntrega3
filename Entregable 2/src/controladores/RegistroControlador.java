8bbe38e04ab656638dbce60094ea1af799459f65
AgustinMoyanoD <agustinmoyano3101@gmail.com>
2024-11-06 16:45:42 -0300

    Sign up recontra fachero

--- Entregable 2/src/controladores/RegistroControlador.java
+++ Entregable 2/src/controladores/RegistroControlador.java
@@ -3,6 +3,8 @@ package controladores;
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -17,10 +19,40 @@ public class RegistroControlador {
 		this.vista = vista;
 		for (JTextComponent tf : vista.devolverCampos()) {
 			tf.addMouseListener(new MouseControlTexts(tf));
+			tf.addFocusListener(new FocusControlTexts(tf));
 		}
 		this.vista.getBotonSalir().addMouseListener(new MouseControlExit());
+		this.vista.getBotonAceptar().addMouseListener(new MouseControlExit());
 		
 	}
+	class FocusControlTexts implements FocusListener{
+		private JTextComponent tf;
+		public FocusControlTexts(JTextComponent tf) {
+			this.tf = tf;
+		}
+		
+		@Override
+		public void focusGained(FocusEvent e) {
+			Color color = tf.getForeground();
+			
+			if (color.equals(new Color(0xAB886D))) {	
+				this.tf.setText(null);
+				this.tf.setForeground(new Color(0x291e17));
+			
+			}
+			return;
+		}
+
+		@Override
+		public void focusLost(FocusEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	/*
+	 * Controlador del botón de salida.
+	 */
 	class MouseControlExit implements MouseListener{
 
 		@Override
@@ -58,21 +90,12 @@ public class RegistroControlador {
 		}
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			try {
-
-				this.tf.setText(null);
-			}catch (NumberFormatException ex) {
-				
-			}
+			//nada
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			try {
-				this.tf.setText(null);
-			}catch (NumberFormatException ex) {
-				
-			}
+			//nada
 		}
 
 		@Override
--- Entregable 2/src/vistas/RegistroVista.java
+++ Entregable 2/src/vistas/RegistroVista.java
@@ -9,19 +9,23 @@ import javax.swing.*;
 import javax.swing.text.JTextComponent;
 
 public class RegistroVista extends JFrame{
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	private JLabel labelTit	 =	new JLabel("Sign Up",SwingConstants.CENTER);
-	private JTextField textFieldName =	new JTextField("Nombre");
-	private JTextField textFieldSur  =	new JTextField("Apellido");
-	private JTextField textFieldMail =	new JTextField("Email");
-	private JPasswordField passField =	new JPasswordField("Contraseña");
+	private JLabel labelTit	=	new JLabel("Sign Up",SwingConstants.CENTER);
+	private JLabel jlName 	= 	new JLabel("Nombre");
+	private JLabel jlSur	= 	new JLabel("Apellido");
+	private JLabel jlMail  	= 	new JLabel("Email");
+	private JLabel jlPass 	= 	new JLabel("Contraseña");
+	
+	private JTextField textFieldName =	new JTextField("Joaquín Victor");
+	private JTextField textFieldSur  =	new JTextField("Gonzales");
+	private JTextField textFieldMail =	new JTextField("buenas@gmail.com");
+	private JPasswordField passField =	new JPasswordField();
 	
 	private JButton botonAceptar = new JButton("Aceptar");
 	private JButton botonSalir 	 = new JButton("X");
 	private JButton botonAtras 	 = new JButton("Atrás");
+
+	
 	public RegistroVista() {
 		
 		//CONFIGURACION DEL JFRAME
@@ -33,57 +37,104 @@ public class RegistroVista extends JFrame{
 		this.setResizable(false);
 		this.setUndecorated(true);
 		this.getContentPane().setBackground(new Color(0xE4E0E1));
-		//CONFIGURACIÓN DE LOS CAMPOS DE TEXTO
-		
+		//CONSTANTES
 		int dimX = this.getWidth();
 		int dimY = this.getHeight();
 		Color camposTextColor = new Color(0xD6C0B3);
+		Color textColor = new Color(0xAB886D);
 		int cons=40;
-		int posIni = dimY/4 + 40;
+		
+		//CONFIGURACIÓN DE LAS ETIQUETAS
+		int posIniY = dimY/4;
+		int posIniX = dimX/2 - 60;
+		
 		//titulo
 		labelTit.setSize(150, 50);
-		labelTit.setLocation(dimX/2-75,dimY/4-40);
+		labelTit.setLocation(dimX/2-75,posIniY-55);
 		labelTit.setFont(new Font("Tahoma", Font.BOLD,30));
 		labelTit.setForeground(new Color(0x291e17));
 		
 		//nombre
-		textFieldName.setBounds(dimX/2-50,posIni,100,20);
-		textFieldName.setBackground(camposTextColor);
-		textFieldName.setBorder(null);
+		jlName.setBounds(posIniX,posIniY,100,20);
 		textFieldName.setFont(new Font("Tahoma",Font.PLAIN,12));
 		
 		//apellido
-		textFieldSur.setBounds(dimX/2-50,posIni+cons,100,20);
+		jlSur.setBounds(posIniX,posIniY+cons,100,20);
+		textFieldName.setFont(new Font("Tahoma",Font.PLAIN,12));
+		
+		//Mail
+		jlMail.setBounds(posIniX,posIniY+cons*2,100,20);
+		textFieldName.setFont(new Font("Tahoma",Font.PLAIN,12));
+		
+		//Contraseña
+		jlPass.setBounds(posIniX,posIniY+cons*3,100,20);
+		textFieldName.setFont(new Font("Tahoma",Font.PLAIN,12));
+				
+		
+		
+		//CONFIGURACIÓN DE LOS CAMPOS DE TEXTO
+		int boxHeight=20;
+		int boxWidth=130;
+		posIniY = dimY/4 + 20;
+		posIniX = dimX/2 - boxWidth/2;
+		
+		//nombre
+		textFieldName.setBounds(posIniX,posIniY,boxWidth,boxHeight);
+		textFieldName.setBackground(camposTextColor);
+		textFieldName.setBorder(null);
+		textFieldName.setFont(new Font("Tahoma",Font.PLAIN,12));
+		textFieldName.setForeground(textColor);
+		
+		
+		//apellido
+		textFieldSur.setBounds(posIniX,posIniY+cons,boxWidth,boxHeight);
 		textFieldSur.setBackground(camposTextColor);
 		textFieldSur.setBorder(null);
 		textFieldSur.setFont(new Font("Tahoma",Font.PLAIN,12));
+		textFieldSur.setForeground(textColor);
 		
 		
 		//mail
-		textFieldMail.setBounds(dimX/2-50,posIni+2*cons,100,20);
+		textFieldMail.setBounds(posIniX,posIniY+2*cons,boxWidth,boxHeight);
 		textFieldMail.setBackground(camposTextColor);
 		textFieldMail.setBorder(null);
 		textFieldMail.setFont(new Font("Tahoma",Font.PLAIN,12));
-		
+		textFieldMail.setForeground(textColor);
 		//contraseña
 		passField.setEchoChar('*');
-		passField.setBounds(dimX/2-50,posIni+3*cons,100,20);
+		passField.setBounds(posIniX,posIniY+3*cons,boxWidth,boxHeight);
 		passField.setBackground(camposTextColor);
 		passField.setBorder(null);
 		passField.setFont(new Font("Tahoma",Font.PLAIN,12));
+		passField.setForeground(textColor);
+		
 		
 		//CONFIGURACIÓN DE BOTONES
 		botonSalir.setBounds(dimX-cons, 10, 30, 30);
 		botonSalir.setBorder(null);
 		botonSalir.setBackground(null);
-		botonSalir.setFont(new Font("Arial", Font.BOLD,20));
+		botonSalir.setFont(new Font("Arial", Font.PLAIN,20));
+		botonSalir.setFocusPainted(false);
+		
+		botonAceptar.setBounds(posIniX,posIniY+4*cons, boxWidth, boxHeight+10);
+		botonAceptar.setBorder(null);
+		botonAceptar.setBackground(new Color(0xAB886D));
+		botonAceptar.setFont(new Font("Arial", Font.PLAIN,20));
+		botonAceptar.setFocusPainted(false);
+		
+		
 		//AGREGRO AL JFRAME
 		this.add(this.labelTit);
 		this.add(this.textFieldName);
 		this.add(this.textFieldSur);
 		this.add(this.textFieldMail);
 		this.add(this.passField);
+		this.add(this.jlSur);
+		this.add(this.jlName);
+		this.add(this.jlMail);
+		this.add(this.jlPass);
 		this.add(botonSalir);
+		this.add(botonAceptar);
 		//
 		this.setVisible(true);
 		
