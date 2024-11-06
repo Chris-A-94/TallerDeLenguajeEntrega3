a924ce7c9b0d26bd5563d29abd8081964853371f
AgustinMoyanoD <agustinmoyano3101@gmail.com>
2024-11-06 12:20:04 -0300

    Sign Up casi completo

--- Entregable 2/src/controladores/RegistroControlador.java
+++ Entregable 2/src/controladores/RegistroControlador.java
@@ -7,6 +7,7 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
 import javax.swing.JTextField;
+import javax.swing.text.JTextComponent;
 
 import vistas.RegistroVista;
 
@@ -14,28 +15,22 @@ public class RegistroControlador {
 	private RegistroVista vista;
 	public RegistroControlador(RegistroVista vista) {
 		this.vista = vista;
-		for (JTextField tf : vista.devolverCampos()) {
-			tf.addMouseListener(new controlMouse(tf));
+		for (JTextComponent tf : vista.devolverCampos()) {
+			tf.addMouseListener(new MouseControlTexts(tf));
 		}
+		this.vista.getBotonSalir().addMouseListener(new MouseControlExit());
+		
 	}
-	class controlMouse implements MouseListener{
-		private JTextField tf;
-		public controlMouse(JTextField tf) {
-			this.tf = tf;
-		}
+	class MouseControlExit implements MouseListener{
+
 		@Override
 		public void mouseClicked(MouseEvent e) {
-			try {
-				this.tf.setText(null);
-			}catch (NumberFormatException ex) {
-				
-			}
+			vista.dispose();
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			// TODO Auto-generated method stub
-			
+			vista.dispose();
 		}
 
 		@Override
@@ -46,12 +41,54 @@ public class RegistroControlador {
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
-			tf.setSelectedTextColor(Color.BLUE);
+			vista.getBotonSalir().setBackground(new Color(0x94847b));
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
+			vista.getBotonSalir().setBackground(new Color(0xE4E0E1));
+		}
+
 		
+	}
+	class MouseControlTexts implements MouseListener{
+		private JTextComponent tf;
+		public MouseControlTexts(JTextComponent tf) {
+			this.tf = tf;
+		}
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			try {
+
+				this.tf.setText(null);
+			}catch (NumberFormatException ex) {
+				
+			}
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+			try {
+				this.tf.setText(null);
+			}catch (NumberFormatException ex) {
+				
+			}
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			tf.setBackground(new Color(0x94847b));
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+			tf.setBackground(new Color(0xD6C0B3));
 		}
 		
 	}
--- Entregable 2/src/vistas/RegistroVista.java
+++ Entregable 2/src/vistas/RegistroVista.java
@@ -1,52 +1,95 @@
 package vistas;
 
 import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.Window;
+import java.awt.Font;
 import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 
 public class RegistroVista extends JFrame{
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private JLabel textFieldTit	 =	new JLabel("Sign in");
-	private JTextField textFieldName =	new JTextField("Nombre",100);
+	private JLabel labelTit	 =	new JLabel("Sign Up",SwingConstants.CENTER);
+	private JTextField textFieldName =	new JTextField("Nombre");
 	private JTextField textFieldSur  =	new JTextField("Apellido");
 	private JTextField textFieldMail =	new JTextField("Email");
 	private JPasswordField passField =	new JPasswordField("Contraseña");
 	
 	private JButton botonAceptar = new JButton("Aceptar");
-	private JButton botonAtras = new JButton("Atrás");
+	private JButton botonSalir 	 = new JButton("X");
+	private JButton botonAtras 	 = new JButton("Atrás");
 	public RegistroVista() {
 		
 		//CONFIGURACION DEL JFRAME
 		this.setTitle("Sign in"); //Titulo del frame
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-		this.setSize(1500,5000); //Tamaño del frame
+		this.setSize(300,350); //Tamaño del frame
 		this.setLocationRelativeTo(null); 
 		this.setLayout(null);
 		this.setResizable(false);
-		this.setBackground(Color.BLUE);
+		this.setUndecorated(true);
+		this.getContentPane().setBackground(new Color(0xE4E0E1));
 		//CONFIGURACIÓN DE LOS CAMPOS DE TEXTO
+		
+		int dimX = this.getWidth();
+		int dimY = this.getHeight();
+		Color camposTextColor = new Color(0xD6C0B3);
+		int cons=40;
+		int posIni = dimY/4 + 40;
+		//titulo
+		labelTit.setSize(150, 50);
+		labelTit.setLocation(dimX/2-75,dimY/4-40);
+		labelTit.setFont(new Font("Tahoma", Font.BOLD,30));
+		labelTit.setForeground(new Color(0x291e17));
+		
+		//nombre
+		textFieldName.setBounds(dimX/2-50,posIni,100,20);
+		textFieldName.setBackground(camposTextColor);
+		textFieldName.setBorder(null);
+		textFieldName.setFont(new Font("Tahoma",Font.PLAIN,12));
+		
+		//apellido
+		textFieldSur.setBounds(dimX/2-50,posIni+cons,100,20);
+		textFieldSur.setBackground(camposTextColor);
+		textFieldSur.setBorder(null);
+		textFieldSur.setFont(new Font("Tahoma",Font.PLAIN,12));
+		
+		
+		//mail
+		textFieldMail.setBounds(dimX/2-50,posIni+2*cons,100,20);
+		textFieldMail.setBackground(camposTextColor);
+		textFieldMail.setBorder(null);
+		textFieldMail.setFont(new Font("Tahoma",Font.PLAIN,12));
+		
+		//contraseña
 		passField.setEchoChar('*');
+		passField.setBounds(dimX/2-50,posIni+3*cons,100,20);
+		passField.setBackground(camposTextColor);
+		passField.setBorder(null);
+		passField.setFont(new Font("Tahoma",Font.PLAIN,12));
 		
-		
+		//CONFIGURACIÓN DE BOTONES
+		botonSalir.setBounds(dimX-cons, 10, 30, 30);
+		botonSalir.setBorder(null);
+		botonSalir.setBackground(null);
+		botonSalir.setFont(new Font("Arial", Font.BOLD,20));
 		//AGREGRO AL JFRAME
-		this.add(this.textFieldTit);
+		this.add(this.labelTit);
 		this.add(this.textFieldName);
 		this.add(this.textFieldSur);
 		this.add(this.textFieldMail);
 		this.add(this.passField);
+		this.add(botonSalir);
 		//
 		this.setVisible(true);
 		
 	}
-	public JLabel getTextFieldTit() {
-		return textFieldTit;
+	public JLabel getlabelTit() {
+		return labelTit;
 	}
 	public JTextField getTextFieldName() {
 		return textFieldName;
@@ -66,11 +109,15 @@ public class RegistroVista extends JFrame{
 	public JButton getBotonAtras() {
 		return botonAtras;
 	}
-	public List<JTextField> devolverCampos(){
-		List<JTextField> listaCampos = new LinkedList<JTextField>();
+	public List<JTextComponent> devolverCampos(){
+		List<JTextComponent> listaCampos = new LinkedList<JTextComponent>();
 		listaCampos.add(textFieldName);
 		listaCampos.add(textFieldSur);
 		listaCampos.add(textFieldMail);
+		listaCampos.add(passField);
 		return listaCampos;
+	}
+	public JButton getBotonSalir() {
+		return botonSalir;
 	}
 }
