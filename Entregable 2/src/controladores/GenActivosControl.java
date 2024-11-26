7b05322334089ae6f0d99f769fe7f592f0819621
Chris <almeidachristian94@hotmail.com>
2024-11-26 19:40:34 -0300

    Funcionamiento basico de generar activos
    No hace nada aun, solo comp visual

--- Entregable 2/src/controladores/GenActivosControl.java
+++ Entregable 2/src/controladores/GenActivosControl.java
@@ -4,14 +4,20 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import daos.UsuarioDAO;
 import entregable1.Usuario;
+import vistas.GenerarActivos;
 
 public class GenActivosControl {
 	
-	private JButton generar;
+	private JButton generar;	
 	private Usuario user;
+	GenerarActivos abrirPanel;
 	
 	public GenActivosControl(JButton generar,Usuario user)
 	{
@@ -26,12 +32,44 @@ public class GenActivosControl {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				UsuarioDAO myUser = new UsuarioDAO();
-				Usuario userMem = myUser.getUsuario(user.getDNI());
-				System.out.println("ola "+userMem.getEmail());				
-			}
-			
+				abrirPanel = new GenerarActivos();	
+				setComportamiento();
+			}			
 		});
+		
+		
 	}
+	
+	private void setComportamiento()
+	{
+		JButton cancelar = abrirPanel.getCancelar();
+		JButton aceptar = abrirPanel.getAceptar();
+		JComboBox<String> opciones = abrirPanel.getOpciones();		
+		JTextField valor = abrirPanel.getValor();	
+		cancelar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				abrirPanel.dispose();
+			}
+		});
+		
+		aceptar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e)
+			{
+				leerData(opciones,valor);
+				abrirPanel.dispose();
+			}
+		});
+		
+	}
+	
+	private void leerData(JComboBox<String> opciones,JTextField valor)
+	{
+		JOptionPane.showMessageDialog(null, "Se cargo "+valor.getText()+" de "+opciones.getSelectedItem().toString(), "Carga Exitosa", JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	
 
 }
---
+++ Entregable 2/src/vistas/GenerarActivos.java
@@ -0,0 +1,94 @@
+package vistas;
+
+import javax.swing.JButton;
+
+import daos.CoinDAO;
+import entregable1.Coin;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.*;
+
+public class GenerarActivos extends JFrame{
+	
+	private JComboBox<String> opciones;
+	private JButton aceptar;
+	private JButton cancelar;
+	private JTextField valor;
+	private JPanel elPanel;
+	
+	public GenerarActivos()
+	{
+		this.setTitle("Generar Activos");
+		this.setSize(300, 200);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setUndecorated(true);
+		this.elPanel = new JPanel();
+		this.elPanel.setLayout(new BoxLayout(elPanel, BoxLayout.Y_AXIS));
+		
+		this.setComponentes();
+		this.add(elPanel);
+		
+		this.setVisible(true);
+	}
+	
+	private void setComponentes()
+	{
+		this.cancelar = new JButton("Cancelar");
+		this.aceptar = new JButton("Aceptar");
+		this.valor = new JTextField();
+		CoinDAO nombres = new CoinDAO();
+		List<String> monedas = new LinkedList<String>();
+		for(Coin moneda: nombres.devolverTabla())
+		{
+			monedas.add(moneda.getNombre());
+		}
+		monedas.add("Todo");
+		this.opciones = new JComboBox<>(monedas.toArray(new String[0]));
+		JLabel mensaje1 = new JLabel("Valor:");
+		JLabel mensaje2 = new JLabel("Elija moneda: ");
+		
+		
+		this.elPanel.add(mensaje1);
+		this.elPanel.add(valor);
+		this.elPanel.add(mensaje2);
+		this.elPanel.add(opciones);
+		this.elPanel.add(aceptar);
+		this.elPanel.add(cancelar);
+	}
+
+	public JComboBox<String> getOpciones() {
+		return opciones;
+	}
+
+	public void setOpciones(JComboBox<String> opciones) {
+		this.opciones = opciones;
+	}
+
+	public JButton getAceptar() {
+		return aceptar;
+	}
+
+	public void setAceptar(JButton aceptar) {
+		this.aceptar = aceptar;
+	}
+
+	public JButton getCancelar() {
+		return cancelar;
+	}
+
+	public void setCancelar(JButton cancelar) {
+		this.cancelar = cancelar;
+	}
+
+	public JTextField getValor() {
+		return valor;
+	}
+
+	public void setValor(JTextField valor) {
+		this.valor = valor;
+	}
+
+}
